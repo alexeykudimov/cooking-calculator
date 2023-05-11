256045d7fd9aa2afc5b24f4a3cd377492c523e34
alexeykudimov <work.alexeykudimov@gmail.com>
2023-05-11 03:26:30 +0300

    get popular components tests

--- tests/test_cooking.py
+++ tests/test_cooking.py
@@ -5,24 +5,34 @@ all_recipes = [
     {"name": "картофель", "value": 50},
     {"name": "огурец", "value": 5},
     {"name": "рыба", "value": 1520},
-    {"name": "яйцо", "value": 50}]
+    {"name": "яйцо", "value": 50},
+    {"name": "бананы", "value": 3}]
 
 all_recipes_but_enough_for_one = [
     {"name": "мясо", "value": 2000},
     {"name": "картофель", "value": 50},
     {"name": "огурец", "value": 1},
     {"name": "рыба", "value": 2000},
-    {"name": "яйцо", "value": 1}]
+    {"name": "яйцо", "value": 1},
+    {"name": "арбуз", "value": 1}]
 
 no_recipes = [
     {"name": "мясо", "value": 1},
     {"name": "рыба", "value": 1},
-    {"name": "ананас", "value": 1}]
+    {"name": "ананас", "value": 1},
+    {"name": "свекла", "value": 1}]
 
 with_repeat_and_uppercase = [
     {"name": "МясО", "value": 250},
     {"name": "мЯсО", "value": 500},
-    {"name": "огурец", "value": 10}]
+    {"name": "огурец", "value": 10},
+    {"name": "кефир", "value": 1},
+    {"name": "гРаНаТ", "value": 1}]
+
+async def test_get_null_popular_components(ac: AsyncClient):
+    response = await ac.get("v1/components/popular")
+
+    assert response.status_code == 418
 
 async def test_no_recipes(ac: AsyncClient):
     response = await ac.post("v1/recipes", json=no_recipes)
@@ -73,3 +83,11 @@ async def test_get_three_recent_recipes(ac: AsyncClient):
 
     assert response.status_code == 200
     assert len(response.json()) == 3
+
+async def test_get_ten_popular_components(ac: AsyncClient):
+    response = await ac.get("v1/components/popular")
+
+    assert response.status_code == 200
+    assert len(response.json()) == 10
+    assert response.json()[0]['name'] == 'мясо'
+    assert response.json()[0]['users_count'] > response.json()[1]['users_count']
